1a3d4fedac9a23912ee1b9db339968e868506c3b
VEK1989 <55896799+VEK1989@users.noreply.github.com>
2021-09-06 12:15:26 +0300

    ie11 cuctomization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'fast-text-encoding/text';
 
 ReactDOM.render(
   <React.StrictMode>
